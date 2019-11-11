ff4d3071f8a2772eb8ada62f8fdb48ddbfc588ef
Sonam Serchan <sonamserchan@192-168-1-110.tpgi.com.au>
2019-11-11 17:59:49 +1100

    First Commit

---
+++ src/build/precache-manifest.f65ff05543788ccfa11688d267b1d6ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dc86ec4e57811b8a7f3ac9733477bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d473c57c3dc3e3ab64ac",
+    "url": "/static/css/main.35c0bd6c.chunk.css"
+  },
+  {
+    "revision": "edf947a1b89f1a855f4a",
+    "url": "/static/js/2.26bba3e0.chunk.js"
+  },
+  {
+    "revision": "d473c57c3dc3e3ab64ac",
+    "url": "/static/js/main.03839334.chunk.js"
+  },
+  {
+    "revision": "16f57bc6dde3f2278832",
+    "url": "/static/js/runtime~main.82f07dd6.js"
+  }
+]);
